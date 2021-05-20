571d07e588a0d02b5e81e43b4dfaf2ede7c8e35c
zmajkic <zeljko.majkic@telekom.de>
2021-05-20 17:02:35 +0200

    getAll hinzugefügt

--- src/main/java/de/telekom/sea2/SeminarApp.java
+++ src/main/java/de/telekom/sea2/SeminarApp.java
@@ -7,7 +7,7 @@
 
 
 package de.telekom.sea2;
-
+import java.util.ArrayList;
 import java.sql.SQLException;
 
 import de.telekom.sea2.model.Person;
@@ -35,6 +35,48 @@ public class SeminarApp {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
+		
+		
+		
+		
+		
+		
+		PersonRepository personRepositoryAll = new PersonRepository();
+		try {
+			personRepositoryAll.dbInit();
+			ArrayList personListAll = personRepositoryAll.getAll();
+	
+			for (int i = 0; i < personListAll.size(); i++) {
+			
+				Person person = (Person)personListAll.get(i);
+				
+				
+		//		System.out.println(person.getFirstname());
+				
+				
+			String string = String.format("%s %s %s %s",
+					person.getId(),
+					person.getSalutation(),
+					person.getFirstname(),
+					person.getLastname()
+					);
+			
+			
+			
+			System.out.println(string);
+			
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
 		
 		
 		
